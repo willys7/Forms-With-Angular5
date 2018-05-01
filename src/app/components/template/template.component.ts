a13ae456a78f1cc656e4999a0d1af968d996e1cc
willys7 <wilfuentes0320@gmail.com>
2018-04-30 22:25:16 -0600

    add validations using bootstrap

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -4,11 +4,11 @@ import {NgForm} from '@angular/forms'
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: [`
-    .ng-invalid.ng-touched:not(form) {
-      border : 1px solid red;
-    }
-  `]
+  // styles: [`
+  //   .ng-invalid.ng-touched:not(form) {
+  //     border : 1px solid red;
+  //   }
+  // `]
 })
 export class TemplateComponent implements OnInit {
   user:any ={
